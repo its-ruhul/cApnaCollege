cb969679f23b3582eac83b9e06783f25beaf9bc8
Ruhul <itsruhul@proton.me>
2025-09-22 19:05:39 +0530

    Just mainting the streak

--- Chapter4/forloop2.c
+++ Chapter4/forloop2.c
@@ -2,10 +2,17 @@
 
 int main() {
 
-    //interator; counter; 
+    //iterator; counter; 
     for(int i = 10; i > 0; i = i - 1){
         printf("%d \n", i);
     }
+
+    //print the numbers from 0 to 10
+    for(int i = 0; i <= 10; i = i + 1){
+        printf("%d", i);
+    }
+
+    printf("\n");
 
     return 0;
 }
