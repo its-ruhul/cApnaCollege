e2c06ab80764dfc061ba4dfc1cc864464815ffdf
Ruhul <itsruhul@proton.me>
2025-11-14 19:53:33 +0530

    pointer-swap

---
+++ Learn/pointer-swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void swap(int *a, int *b);
+
+void swap(int *a, int *b) {
+
+  int temp = *a;
+
+  *a = *b;
+  *b = temp;
+}
+
+int main() {
+
+  int x = 10;
+  int y = 20;
+
+  printf("BEFORE SWAP: x = %d, y = %d \n", x, y);
+
+  swap(&x, &y);
+  printf("AFTER SWAP: x = %d, y = %d \n", x, y);
+  
+  return 0;
+}
